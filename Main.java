c0c61b494d6a95796f1b94a82dbff623dd74aed6
Joaquim Pessôa Filho <joaquimp@gmail.com>
2023-02-07 08:11:09 -0300

    First commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+class Main {
+  public static void main(String[] args) {
+    AppCaixa1.main(args);
+    // AppBanco1.main(args);
+  }
+}
